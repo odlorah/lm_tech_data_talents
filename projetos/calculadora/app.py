819c23fcda98f70cd0b889a595cd4eb4e73e68b0
Haroldo Viana <haroldo_viana@hotmail.com>
2023-04-03 22:22:39 -0300

    Adicionando código para testar a funcionalidade

---
+++ projetos/calculadora/app.py
@@ -0,0 +1,8 @@
+# Importanto a função do arquivo calculadora/__init__.py
+import os
+import sys
+sys.path.insert(0, os.getcwd())
+from projetos.calculadora import calcule
+
+# Executando a aplicação
+print(calcule())
